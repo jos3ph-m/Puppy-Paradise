9c86ee49adb93e299f922c5015ee316779eb8d41
Joseph M <jmedina1985@hotmail.com>
2021-02-05 14:10:56 -0800

    Working on data - 3

--- src/components/DogData.js
+++ src/components/DogData.js
@@ -1,3 +1,12 @@
 import React from 'react';
 
-export const DogData = [];
+export const DogData = [
+  {
+    breed: 'Pug',
+    path: '/pug',
+    origin: 'China',
+    description:
+      'The pug is a breed of dog distinguished by a wrinkly, short-muzzled face and curled tail. The breed has a fine, glossy coat that comes in a variety of colours, most often light brown (fawn) or black, and a compact, square body with well-developed muscles.',
+    image: '../img/pug.jpg',
+  },
+];
