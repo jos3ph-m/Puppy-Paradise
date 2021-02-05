960709cca11a860eca4f79d3abd226f70eb87929
Joseph M <jmedina1985@hotmail.com>
2021-02-04 21:37:16 -0800

    Getting started - 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 
+import Nav from './components/Nav';
+
 function App() {
   return (
     <div className="App">
+      <Nav></Nav>
       <h1>Puppy Paradise 🐕</h1>
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      <h2>A nav will go here</h2>
+    </div>
+  );
+};
+
+export default Nav;
