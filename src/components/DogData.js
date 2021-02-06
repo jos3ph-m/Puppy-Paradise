67d81b420dfad273123f2590e891f42bd23f96e2
Joseph M <jmedina1985@hotmail.com>
2021-02-05 17:32:45 -0800

    Working on data - 7

--- src/components/DogData.js
+++ src/components/DogData.js
@@ -5,7 +5,7 @@ export const DogData = [
     breed: 'Pug',
     path: '/pug',
     origin: 'China',
-    colors: ['fawn', 'black'], 
+    colors: "Fawn, Apricot, and Black", 
     description:
       'The pug is a breed of dog distinguished by a wrinkly, short-muzzled face and curled tail. The breed has a fine, glossy coat that comes in a variety of colours, most often light brown (fawn) or black, and a compact, square body with well-developed muscles.',
     image: '../img/pug.jpg',
@@ -14,7 +14,7 @@ export const DogData = [
     breed: 'Labrador Retriever',
     path: '/labrador',
     origin: 'United Kingdom',
-    colors: ['yellow', 'chocolate', 'black']
+    colors: "Yellow, White, Chocolate, Silver, and Black"
     description:
       'The Labrador Retriever, often abbreviated to Labrador, is a breed of retriever-gun dog from the United Kingdom that was developed from imported Canadian fishing dogs. The Labrador is one of the most popular dog breeds in a number of countries in the world, particularly in the Western world.',
     image: '../img/labrador.jpg',
@@ -23,7 +23,7 @@ export const DogData = [
     breed: 'Beagle',
     path: '/beagle',
     origin: 'England',
-    colors: ['Beagle color variations can include blue, black, tan, white, red, lemon, red and brown'],
+    colors: ['Color variations can include combinations of blue, black, tan, white, red, lemon, red and brown'],
     description:
       'The beagle is a breed of small hound that is similar in appearance to the much larger foxhound. The beagle is a scent hound, developed primarily for hunting hare (beagling). Possessing a great sense of smell and superior tracking instincts, the beagle is the primary breed used as detection dogs for prohibited agricultural imports and foodstuffs in quarantine around the world. The beagle is intelligent. It is a popular pet due to its size, good temper, and a lack of inherited health problems.',
     image: '../img/beagle.jpg',
