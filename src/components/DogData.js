e28fc62fe5147ebce59d08b06d7edfa2719be696
Joseph M <jmedina1985@hotmail.com>
2021-02-05 19:09:58 -0800

    Working on data - 10

--- src/components/DogData.js
+++ src/components/DogData.js
@@ -23,7 +23,7 @@ export const DogData = [
     breed: 'Beagle',
     path: '/beagle',
     origin: 'England',
-    colors: ['Tri-color, Lemon & White, White & Tan, Chocolate Tri, Orange & White, Red & White, and White & Chocolate'],
+    colors: "Tri-color, Lemon & White, White & Tan, Chocolate Tri, Orange & White, Red & White, and White & Chocolate",
     description:
       'The beagle is a breed of small hound that is similar in appearance to the much larger foxhound. The beagle is a scent hound, developed primarily for hunting hare (beagling). Possessing a great sense of smell and superior tracking instincts, the beagle is the primary breed used as detection dogs for prohibited agricultural imports and foodstuffs in quarantine around the world. The beagle is intelligent. It is a popular pet due to its size, good temper, and a lack of inherited health problems.',
     image: '../img/beagle.jpg',
@@ -32,7 +32,7 @@ export const DogData = [
     breed: 'German Shepherd',
     path: '/german',
     origin: 'Germany',
-    colors: ['Black, Sable, Grey, Black & Silver, Red & Black, and Black & Tan'],
+    colors: "Black, Sable, Grey, Black & Silver, Red & Black, and Black & Tan",
     description:
       'The German Shepherd is a breed of medium to large-sized working dog that originated in Germany. According to the FCI, the breed\'s English language name is German Shepherd Dog. The breed was officially[who?] known as the "Alsatian Wolf Dog" in the UK from after the First World War until 1977 when its name was changed back to German Shepherd. Despite its wolf-like appearance, the German Shepherd is a relatively modern breed of dog, with their origin dating to 1899.',
     image: '../img/german.jpg',
@@ -41,7 +41,7 @@ export const DogData = [
     breed: 'Basset Hound',
     path: '/basset',
     origin: 'United Kingdom',
-    colors: ['Tri-color, Lemon & White, Black & White, Red & White, Black & Brown, and White & Chocolate'],
+    colors: "Tri-color, Lemon & White, Black & White, Red & White, Black & Brown, and White & Chocolate",
     description:
       'The Basset Hound is a short-legged breed of dog in the hound family. The Basset is a scent hound that was originally bred for the purpose of hunting hare. Their sense of smell and ability to ground-scent is second only to the Bloodhound.',
     image: '../img/basset.jpg',
@@ -50,10 +50,19 @@ export const DogData = [
     breed: 'Dalmatian',
     path: '/dalmatian',
     origin: 'Croatia',
-    colors: ['Black & White, and Liver & White'],
+    colors: "Black & White, and Liver & White",
     description:
-      'TThe Dalmatian is a breed of large-sized dog, noted for its unique white coat marked with black or liver-colored spots. Originating as a hunting dog, it was also used as a carriage dog in its early days. The origins of this breed can be traced back to Croatia and its historical region of Dalmatia. It is thought that early ancestors of the breed were certain breeds of pointers and a spotted Great Dane. Today, it is a popular family pet and many dog enthusiasts enter Dalmatians into kennel club competitions.',
+      'The Dalmatian is a breed of large-sized dog, noted for its unique white coat marked with black or liver-colored spots. Originating as a hunting dog, it was also used as a carriage dog in its early days. The origins of this breed can be traced back to Croatia and its historical region of Dalmatia. It is thought that early ancestors of the breed were certain breeds of pointers and a spotted Great Dane. Today, it is a popular family pet and many dog enthusiasts enter Dalmatians into kennel club competitions.',
     image: '../img/dalmatian.jpg',
+  },
+  {
+    breed: 'Bernese Mountain Dog',
+    path: '/bernese',
+    origin: 'Switzerland',
+    colors: "Tri-color",
+    description:
+      'The Bernese Mountain Dog is a large dog breed, one of the four breeds of Sennenhund-type dogs from the Swiss Alps. These dogs have roots in the Roman mastiffs. The name Sennenhund is derived from the German Senne ("alpine pasture") and Hund (hound/dog), as they accompanied the alpine herders and dairymen called Senn. Berner (or Bernese in English) refers to the area of the breed\'s origin, in the canton of Bern. This breed was originally kept as a general farm dog. Large Sennenhund in the past were also used as draft animals, pulling carts. The breed was officially established in 1912.',
+    image: '../img/bernese.jpg',
   },
 
 ];
