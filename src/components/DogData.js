5c9eb1110226a580832d8f352208646bd19f6d97
Joseph M <jmedina1985@hotmail.com>
2021-02-05 16:22:12 -0800

    Working on data - 4

--- src/components/DogData.js
+++ src/components/DogData.js
@@ -9,4 +9,12 @@ export const DogData = [
       'The pug is a breed of dog distinguished by a wrinkly, short-muzzled face and curled tail. The breed has a fine, glossy coat that comes in a variety of colours, most often light brown (fawn) or black, and a compact, square body with well-developed muscles.',
     image: '../img/pug.jpg',
   },
+  {
+    breed: 'Labrador Retriever',
+    path: '/labrador',
+    origin: 'United Kingdom',
+    description:
+      'The Labrador Retriever, often abbreviated to Labrador, is a breed of retriever-gun dog from the United Kingdom that was developed from imported Canadian fishing dogs. The Labrador is one of the most popular dog breeds in a number of countries in the world, particularly in the Western world.',
+    image: '../img/labrador.jpg',
+  },
 ];
