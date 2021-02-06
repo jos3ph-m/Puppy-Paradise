e06f85ed9397405d2fb14b89a5a2793d92888c03
Joseph M <jmedina1985@hotmail.com>
2021-02-06 11:36:23 -0800

    Bringing data into Dogs - 1

--- src/components/DogData.js
+++ src/components/DogData.js
@@ -5,7 +5,7 @@ export const DogData = [
     breed: 'Pug',
     path: '/pug',
     origin: 'China',
-    colors: "Fawn, Apricot, and Black", 
+    colors: 'Fawn, Apricot, and Black',
     description:
       'The pug is a breed of dog distinguished by a wrinkly, short-muzzled face and curled tail. The breed has a fine, glossy coat that comes in a variety of colours, most often light brown (fawn) or black, and a compact, square body with well-developed muscles.',
     image: '../img/pug.jpg',
@@ -14,7 +14,7 @@ export const DogData = [
     breed: 'Labrador Retriever',
     path: '/labrador',
     origin: 'United Kingdom',
-    colors: "Yellow, White, Chocolate, Silver, and Black"
+    colors: 'Yellow, White, Chocolate, Silver, and Black',
     description:
       'The Labrador Retriever, often abbreviated to Labrador, is a breed of retriever-gun dog from the United Kingdom that was developed from imported Canadian fishing dogs. The Labrador is one of the most popular dog breeds in a number of countries in the world, particularly in the Western world.',
     image: '../img/labrador.jpg',
@@ -23,7 +23,8 @@ export const DogData = [
     breed: 'Beagle',
     path: '/beagle',
     origin: 'England',
-    colors: "Tri-color, Lemon & White, White & Tan, Chocolate Tri, Orange & White, Red & White, and White & Chocolate",
+    colors:
+      'Tri-color, Lemon & White, White & Tan, Chocolate Tri, Orange & White, Red & White, and White & Chocolate',
     description:
       'The beagle is a breed of small hound that is similar in appearance to the much larger foxhound. The beagle is a scent hound, developed primarily for hunting hare (beagling). Possessing a great sense of smell and superior tracking instincts, the beagle is the primary breed used as detection dogs for prohibited agricultural imports and foodstuffs in quarantine around the world. The beagle is intelligent. It is a popular pet due to its size, good temper, and a lack of inherited health problems.',
     image: '../img/beagle.jpg',
@@ -32,7 +33,7 @@ export const DogData = [
     breed: 'German Shepherd',
     path: '/german',
     origin: 'Germany',
-    colors: "Black, Sable, Grey, Black & Silver, Red & Black, and Black & Tan",
+    colors: 'Black, Sable, Grey, Black & Silver, Red & Black, and Black & Tan',
     description:
       'The German Shepherd is a breed of medium to large-sized working dog that originated in Germany. According to the FCI, the breed\'s English language name is German Shepherd Dog. The breed was officially[who?] known as the "Alsatian Wolf Dog" in the UK from after the First World War until 1977 when its name was changed back to German Shepherd. Despite its wolf-like appearance, the German Shepherd is a relatively modern breed of dog, with their origin dating to 1899.',
     image: '../img/german.jpg',
@@ -41,7 +42,8 @@ export const DogData = [
     breed: 'Basset Hound',
     path: '/basset',
     origin: 'United Kingdom',
-    colors: "Tri-color, Lemon & White, Black & White, Red & White, Black & Brown, and White & Chocolate",
+    colors:
+      'Tri-color, Lemon & White, Black & White, Red & White, Black & Brown, and White & Chocolate',
     description:
       'The Basset Hound is a short-legged breed of dog in the hound family. The Basset is a scent hound that was originally bred for the purpose of hunting hare. Their sense of smell and ability to ground-scent is second only to the Bloodhound.',
     image: '../img/basset.jpg',
@@ -50,7 +52,7 @@ export const DogData = [
     breed: 'Dalmatian',
     path: '/dalmatian',
     origin: 'Croatia',
-    colors: "Black & White, and Liver & White",
+    colors: 'Black & White, and Liver & White',
     description:
       'The Dalmatian is a breed of large-sized dog, noted for its unique white coat marked with black or liver-colored spots. Originating as a hunting dog, it was also used as a carriage dog in its early days. The origins of this breed can be traced back to Croatia and its historical region of Dalmatia. It is thought that early ancestors of the breed were certain breeds of pointers and a spotted Great Dane. Today, it is a popular family pet and many dog enthusiasts enter Dalmatians into kennel club competitions.',
     image: '../img/dalmatian.jpg',
@@ -59,7 +61,7 @@ export const DogData = [
     breed: 'Bernese Mountain Dog',
     path: '/bernese',
     origin: 'Switzerland',
-    colors: "Tri-color",
+    colors: 'Tri-color',
     description:
       'The Bernese Mountain Dog is a large dog breed, one of the four breeds of Sennenhund-type dogs from the Swiss Alps. These dogs have roots in the Roman mastiffs. The name Sennenhund is derived from the German Senne ("alpine pasture") and Hund (hound/dog), as they accompanied the alpine herders and dairymen called Senn. Berner (or Bernese in English) refers to the area of the breed\'s origin, in the canton of Bern. This breed was originally kept as a general farm dog. Large Sennenhund in the past were also used as draft animals, pulling carts. The breed was officially established in 1912.',
     image: '../img/bernese.jpg',
@@ -68,7 +70,8 @@ export const DogData = [
     breed: 'English Bulldog',
     path: '/bulldog',
     origin: 'England',
-    colors: "Red, White, Fawn, Fallow, Brindle, Chocolate, Black, Tri-Color, Blue, Lilac",
+    colors:
+      'Red, White, Fawn, Fallow, Brindle, Chocolate, Black, Tri-Color, Blue, Lilac',
     description:
       'The Bernese Mountain Dog is a large dog breed, one of the four breeds of Sennenhund-type dogs from the Swiss Alps. These dogs have roots in the Roman mastiffs. The name Sennenhund is derived from the German Senne ("alpine pasture") and Hund (hound/dog), as they accompanied the alpine herders and dairymen called Senn. Berner (or Bernese in English) refers to the area of the breed\'s origin, in the canton of Bern. This breed was originally kept as a general farm dog. Large Sennenhund in the past were also used as draft animals, pulling carts. The breed was officially established in 1912.',
     image: '../img/bulldog.jpg',
@@ -77,10 +80,10 @@ export const DogData = [
     breed: 'Rough Collie',
     path: '/collie',
     origin: 'United Kingdom',
-    colors: "Sable, Mahogany Sable, Shaded Sable, Tri-color, Blue Merle, Sable Merle, Colour Headed White, Double Merle, and White",
+    colors:
+      'Sable, Mahogany Sable, Shaded Sable, Tri-color, Blue Merle, Sable Merle, Colour Headed White, Double Merle, and White',
     description:
       'The Rough Collie (also known as the Long-Haired Collie) is a long-coated dog breed of medium to large size that, in its original form, was a type of collie used and bred for herding sheep in Scotland. More recent breeding has focused on the Collie as a show dog, and also companion. The breed specifications call for a distinctive long narrow tapered snout and tipped (semiprick) ears, so some dogs must have their ears taped when young.',
     image: '../img/collie.jpg',
   },
-
 ];
