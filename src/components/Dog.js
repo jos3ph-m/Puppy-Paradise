74850e6c4a283091870aa0986796f91fb31991a2
Joseph M <jmedina1985@hotmail.com>
2021-02-06 21:38:36 -0800

    Dog grid in action

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { DogData } from './DogData';
 
+const Dog = styled.div``;
+
+const DogsContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto;
+`;
+
 const Dogs = () => {
   return (
-    <div>
-      <div>
-        {DogData.map((dog, index) => {
-          return (
-            <div key={index}>
-              <h2>{dog.breed}</h2>
-              <h4>{dog.origin}</h4>
-              <p>{dog.colors}</p>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <DogsContainer>
+      {DogData.map((dog, index) => {
+        return (
+          <div key={index}>
+            <h2>{dog.breed}</h2>
+            <h4>{dog.origin}</h4>
+            <p>{dog.colors}</p>
+          </div>
+        );
+      })}
+    </DogsContainer>
   );
 };
 
